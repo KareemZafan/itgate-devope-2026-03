6eaa42a634b09f29853df498968482c8856c3606
Kareem Mohamed <kareem.mohamed@adfinis.com>
2026-06-13 21:24:35 +0300

    Adding add function

--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
 #inlcude <stdio.h> 
 
+double add(double a , double b){
+ return a + b; 
+}
+
 
 int main(void){
 
  printf("Hello World!"); 
+ double a = 100; 
+ double b = 150; 
+ printf("Summation = %d",add(a,b)); 
 
 return 0; 
 }
